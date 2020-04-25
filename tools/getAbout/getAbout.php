2eb98d6979beada51e3354a99c6ba0ee1e7d4be5
*ERR0R* <59941931+Err0r-ICA@users.noreply.github.com>
2020-04-25 21:06:42 +0200

    Update getAbout.php

--- tools/getAbout/getAbout.php
+++ tools/getAbout/getAbout.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Author  : Wahyu Arif Purnomo
- * Name    : Facebook Toolkit++
- * Version : 1.4
- * Update  : 12 June 2019
+
+
+
+
  * 
  * If you are a reliable programmer or the best developer, please don't change anything.
  * If you want to be appreciated by others, then don't change anything in this script.
